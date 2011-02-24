749b6da15dba882462726e49ec37f27a16a4bd9b
zoe somebody <me@beingzoe.com>
2011-02-24 15:25:06 -0800

    Improved plugin dependence by creating a bootstrapping concept protecting the theme from completely falling apart if KST is not installed.

--- functions.php
+++ functions.php
@@ -1,4 +1,4 @@
-<?php
+﻿<?php
 /**
  * Twenty Eleven Parent Theme based on Kitchen Sink HTML5 Base
  *
@@ -13,6 +13,7 @@
  * @version     0.1
  * @since       0.1
 */
+include "_kst_bootstrap_theme.php"; // Protects theme from Kitchen Sink HTML5 not being installed and activated
 
 // KST Base theme settings array
 // Various Kitchen Sink HTML5 Base settings for your theme
@@ -29,140 +30,120 @@ $twenty_eleven_settings = array(
     'theme_seo_title_sep'       => '&laquo;',               // Separator between title bar title segments
 );
 
-// Temp while making seo class
-define('KST_SEO_TITLE_SEPARATOR_DEFAULT', '&laquo;');
+
+// REGISTER YOUR THEME WITH KITCHEN SINK HTML5 BASE
+$my_theme = new KST_Kitchen_Theme($twenty_eleven_settings, 'and_the_kitchen_sink');
+
+// LOAD A PRESET CONFIGURATION - default, minimum, and_the_kitchen_sink
+// You may alternatively pass the preset value as a 2nd argument when invoking your kitchen above and delete this method call (as we did above)
+// OR GO ALA CARTE - Not everybody likes presets so load what you want
+// Load preset example: $my_theme->loadPreset('and_the_kitchen_sink');
+// Load preset example: $my_theme->load('wp_sensible_defaults');
+
+// An array to create a theme options page - make one array per options page you need
+$twentyeleven_options = array(
+    'parent_slug'           => 'kst',
+    'menu_title'            => 'Layout Settings',
+    'page_title'            => 'Custom Layout Settings',
+    'capability'            => 'manage_options',
+    'view_page_callback'    => "auto",
+    'options'               => array(
+            'misc_text_section' => array(
+                            "name"      => 'Misc text options',
+                            "desc"      => "<p><em>Edit various text in the main layout</em></p>",
+                            "type"      => "section",
+                            "is_shut"   => FALSE
+                            ),
+            'copyright_notice' => array(
+                            "name"      => 'Copyright notice',
+                            "desc"      => 'Appears at the bottom of every page.',
+                            "default"   => "Copyright © " . date('Y') . ", All Rights Reserved",
+                            "type"      => "text",
+                            "size"      => "150"
+                            ),
+            'misc_contact_section' => array(
+                            "name"      => 'Misc contact options',
+                            "desc"      => "<p><em>Various contact options for forms etc...</em></p>",
+                            "type"      => "section",
+                            "is_shut"   => FALSE
+                            ),
+            'main_contact_form_section' => array(
+                            "name"      => 'Primary site contact form',
+                            "desc"      => "<p><em>Settings for the main contact form.</em></p>",
+                            "type"      => "subsection",
+                            "is_shut"   => FALSE
+                            ),
+            'contact_primary_to_address' => array(
+                            "name"      => '"To" address',
+                            "desc"      => 'Email address to deliver mail from the main contact form',
+                            "default"   => "test@example.org",
+                            "type"      => "text",
+                            "size"      => "150"
+                            ),
+            'sample_section' => array(
+                            "name"      => 'Sample Options',
+                            "desc"      => "<p><em>Sample options for layout and presentation</em></p>",
+                            "type"      => "section",
+                            "is_shut"   => FALSE
+                            ),
+            'TEST_SELECT' => array(
+                            "name"    => __('PLUGIN Select'),
+                            "desc"    => __("There are many choices awaiting"),
+                            "default"     => "Select 2",
+                            "type"    => "select",
+                            'multi'     => TRUE,
+                            'size'      => 3,
+                            'form_attr' => 'style="height: auto;"',
+                            "options" => array(
+                                                "Select 1" => 10,
+                                                "Select 2" => 20,
+                                                "Select 3" => 30,
+                                                "Select 4" => 40,
+                                                "Select 5" => 50
+                                                )
+                            ),
+            'sample_wp_categories' => array(
+                            "name"      => 'Featured Category',
+                            "desc"      => 'This doesn\'t really do anything but demonstrates a select_wp_categories option block',
+                            "default"   => "Bob",
+                            "type"      => "select_wp_categories",
+                            "args"      => array( )
+                            ),
+            'sample_checkbox' => array(
+                            "name"      => 'Turn something on',
+                            "desc"      => "Checked by default.",
+                            "default"   => TRUE,
+                            "type"      => "checkbox",
+                            )
+            )
+    );
 
 
-if ( class_exists('KST') ) {
+// CREATE ADMIN OPTIONS MENUS/PAGES - Don't forget to make an array
+$my_theme->load('options');
+$my_theme->options->add($twentyeleven_options);
 
-    // REGISTER YOUR THEME WITH KITCHEN SINK HTML5 BASE
-    $my_theme = new KST_Kitchen_Theme($twenty_eleven_settings, 'and_the_kitchen_sink');
+// Use some of the nifty WordPress function replacements for a big time saver (and a cleaner kitchen)
+$my_theme->wordpress->registerSidebar('Blog Sidebar', 'Sidebar content for blog articles');
+$my_theme->wordpress->registerSidebar('Pages Sidebar', 'Sidebar content for pages');
+$my_theme->wordpress->registerSidebar('Home Sidebar', 'Sidebar content for home page');
+$my_theme->wordpress->registerSidebars(4, 'Footer Area');
 
-    // LOAD A PRESET CONFIGURATION - default, minimum, and_the_kitchen_sink
-    // You may alternatively pass the preset value as a 2nd argument when invoking your kitchen above and delete this method call (as we did above)
-    // OR GO ALA CARTE - Not everybody likes presets so load what you want
-    // Load preset example: $my_theme->loadPreset('and_the_kitchen_sink');
-    // Load preset example: $my_theme->load('wp_sensible_defaults');
+$my_theme->load('additional_image_sizes');
 
-    // An array to create a theme options page - make one array per options page you need
-    $twentyeleven_options = array(
-        'parent_slug'           => 'kst',
-        'menu_title'            => 'Layout Settings',
-        'page_title'            => 'Custom Layout Settings',
-        'capability'            => 'manage_options',
-        'view_page_callback'    => "auto",
-        'options'               => array(
-                'misc_text_section' => array(
-                                "name"      => 'Misc text options',
-                                "desc"      => "<p><em>Edit various text in the main layout</em></p>",
-                                "type"      => "section",
-                                "is_shut"   => FALSE
-                                ),
-                'copyright_notice' => array(
-                                "name"      => 'Copyright notice',
-                                "desc"      => 'Appears at the bottom of every page.',
-                                "default"   => "Copyright © " . date('Y') . ", All Rights Reserved",
-                                "type"      => "text",
-                                "size"      => "150"
-                                ),
-                'misc_contact_section' => array(
-                                "name"      => 'Misc contact options',
-                                "desc"      => "<p><em>Various contact options for forms etc...</em></p>",
-                                "type"      => "section",
-                                "is_shut"   => FALSE
-                                ),
-                'main_contact_form_section' => array(
-                                "name"      => 'Primary site contact form',
-                                "desc"      => "<p><em>Settings for the main contact form.</em></p>",
-                                "type"      => "subsection",
-                                "is_shut"   => FALSE
-                                ),
-                'contact_primary_to_address' => array(
-                                "name"      => '"To" address',
-                                "desc"      => 'Email address to deliver mail from the main contact form',
-                                "default"   => "test@example.org",
-                                "type"      => "text",
-                                "size"      => "150"
-                                ),
-                'sample_section' => array(
-                                "name"      => 'Sample Options',
-                                "desc"      => "<p><em>Sample options for layout and presentation</em></p>",
-                                "type"      => "section",
-                                "is_shut"   => FALSE
-                                ),
-                'TEST_SELECT' => array(
-                                "name"    => __('PLUGIN Select'),
-                                "desc"    => __("There are many choices awaiting"),
-                                "default"     => "Select 2",
-                                "type"    => "select",
-                                'multi'     => TRUE,
-                                'size'      => 3,
-                                'form_attr' => 'style="height: auto;"',
-                                "options" => array(
-                                                    "Select 1" => 10,
-                                                    "Select 2" => 20,
-                                                    "Select 3" => 30,
-                                                    "Select 4" => 40,
-                                                    "Select 5" => 50
-                                                    )
-                                ),
-                'sample_wp_categories' => array(
-                                "name"      => 'Featured Category',
-                                "desc"      => 'This doesn\'t really do anything but demonstrates a select_wp_categories option block',
-                                "default"   => "Bob",
-                                "type"      => "select_wp_categories",
-                                "args"      => array( )
-                                ),
-                'sample_checkbox' => array(
-                                "name"      => 'Turn something on',
-                                "desc"      => "Checked by default.",
-                                "default"   => TRUE,
-                                "type"      => "checkbox",
-                                )
-                )
+// Jit Message Settings
+$jit_message_settings = array( //array(&$this, 'callbackInit')
+            'content_source'    => 'posts', // posts|or_valid_callback; use 'or_valid_callback' where you will use separate logic to determine message and the site/blog owner can't choose per post/page/custom
+            'trigger'           => '.wp_entry', // .wp_entry - .wp_entry_footer, #ft
+            'wrapper'           => '#jit_box',
+            'side'              => 'right',
+            'top_or_bottom'     => 'bottom',
+            'speed_in'          => 300,
+            'speed_out'         => 100
         );
 
+$my_theme->jit_message->add($jit_message_settings);
 
-    // CREATE ADMIN OPTIONS MENUS/PAGES - Don't forget to make an array
-    $my_theme->load('options');
-    $my_theme->options->add($twentyeleven_options);
-
-    // Use some of the nifty WordPress function replacements for a big time saver (and a cleaner kitchen)
-    $my_theme->wordpress->registerSidebar('Blog Sidebar', 'Sidebar content for blog articles');
-    $my_theme->wordpress->registerSidebar('Pages Sidebar', 'Sidebar content for pages');
-    $my_theme->wordpress->registerSidebar('Home Sidebar', 'Sidebar content for home page');
-    $my_theme->wordpress->registerSidebars(4, 'Footer Area');
-
-    $my_theme->load('additional_image_sizes');
-
-    // Jit Message Settings
-    $jit_message_settings = array( //array(&$this, 'callbackInit')
-                'content_source'    => 'posts', // posts|or_valid_callback; use 'or_valid_callback' where you will use separate logic to determine message and the site/blog owner can't choose per post/page/custom
-                'trigger'           => '.wp_entry', // .wp_entry - .wp_entry_footer, #ft
-                'wrapper'           => '#jit_box',
-                'side'              => 'right',
-                'top_or_bottom'     => 'bottom',
-                'speed_in'          => 300,
-                'speed_out'         => 100
-            );
-
-    $my_theme->jit_message->add($jit_message_settings);
-
-    // Any other KST dependent code should be here to protect it in case the "KST plugin framework" is removed
-
-
-
-
-} else {
-    // Needs to check if it is in the admin section OR in the login page (login is not in the admin)
-    if ( is_admin() ) {
-        return;
-    } else {
-        // Having a FUN and useful help message would be cool.
-    echo "<h1>Pretty cool!<br />You are using a Kitchen Sink based WordPress theme<br />HOWEVER...</h1><p>...you have not activated the KST Plugin in WordPress OR you haven't included it as library in your theme.<br />See the <a href='http://beingzoe.com/zui/wordpress/kitchen_sink_theme'>documentation</a> if you need assistance.</p><p><a href='#'>Sign in</a> to WordPress.";
-        exit;
-    }
-}
 
 /*
  * Now just do whatever normal WordPress themey things you would do
@@ -264,3 +245,8 @@ function kst_add_custom_image_header() {
 function kst_add_custom_image_header_admin() {
 
 }
+ ?>
+
+
+
+
